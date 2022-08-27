bbcf1535dc0f193c81853e4ba84ac20129e089a3
Alexis Mainwaring <leximainw@gmail.com>
2022-08-27 12:50:40 -0400

    correct ':' to '=' in Models and create new initial migrations

--- api_snippets/models.py
+++ api_snippets/models.py
@@ -4,31 +4,31 @@ from core.models import User
 
 
 class Language(models.Model):
-    name: models.CharField(max_length=200)
-    hljs_name: models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
+    hljs_name = models.CharField(max_length=200)
 
 
 class SnippetPermissionSet(models.Model):
-    delete: models.BooleanField()
-    edit: models.BooleanField()
-    edit_perms: models.BooleanField()
-    fork: models.BooleanField()
-    rename: models.BooleanField()
-    view: models.BooleanField()
+    delete = models.BooleanField()
+    edit = models.BooleanField()
+    edit_perms = models.BooleanField()
+    fork = models.BooleanField()
+    rename = models.BooleanField()
+    view = models.BooleanField()
 
 
 class Snippet(models.Model):
-    title: models.CharField(max_length=100)
-    author: models.ForeignKey(User, on_delete=models.CASCADE, related_name='snippets')
-    description: models.TextField(max_length=2000)
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='snippets')
+    description = models.TextField(max_length=2000)
     created_at = models.DateTimeField(auto_now_add=True)
-    anyone_can: SnippetPermissionSet()
-    language: models.ForeignKey(Language, on_delete=models.SET_NULL, related_name='snippets', blank=True, null=True)
-    language_version: SemVerField()
-    code: models.TextField()
+    anyone_can = SnippetPermissionSet()
+    language = models.ForeignKey(Language, on_delete=models.SET_NULL, related_name='snippets', blank=True, null=True)
+    language_version = SemVerField()
+    code = models.TextField()
 
 
-class SnippetPermissions(models.Model):
-    user: models.ForeignKey(User, on_delete=models.CASCADE, related_name='snippetperms')
-    snippet: models.ForeignKey(Snippet, on_delete=models.CASCADE, related_name='snippetperms')
-    can: SnippetPermissionSet()
+class SnippetUserPermission(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='snippetperms')
+    snippet = models.ForeignKey(Snippet, on_delete=models.CASCADE, related_name='snippetperms')
+    can = SnippetPermissionSet()
