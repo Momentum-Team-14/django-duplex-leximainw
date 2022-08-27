e8f94f3baa8ebf25383995d040aefc2777c42346
Alexis Mainwaring <leximainw@gmail.com>
2022-08-27 14:06:51 -0400

    allow Snippets to have no description or language version

--- api_snippets/models.py
+++ api_snippets/models.py
@@ -20,11 +20,11 @@ class SnippetPermissionSet(models.Model):
 class Snippet(models.Model):
     title = models.CharField(max_length=100)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='snippets')
-    description = models.TextField(max_length=2000)
+    description = models.TextField(max_length=2000, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     anyone_can = SnippetPermissionSet()
     language = models.ForeignKey(Language, on_delete=models.SET_NULL, related_name='snippets', blank=True, null=True)
-    language_version = SemVerField()
+    language_version = SemVerField(blank=True, null=True)
     code = models.TextField()
 
 
