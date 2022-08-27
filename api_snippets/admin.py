eaf14f2fe7fc872cd03079ac9b577154cbb5abc1
Alexis Mainwaring <leximainw@gmail.com>
2022-08-27 14:35:54 -0400

    add string methods to models

--- api_snippets/admin.py
+++ api_snippets/admin.py
@@ -3,4 +3,5 @@ from . import models
 
 admin.site.register(models.Language)
 admin.site.register(models.Snippet)
+admin.site.register(models.SnippetPermissionSet)
 admin.site.register(models.SnippetUserPermission)
--- api_snippets/models.py
+++ api_snippets/models.py
@@ -7,14 +7,28 @@ class Language(models.Model):
     name = models.CharField(max_length=200)
     hljs_name = models.CharField(max_length=200)
 
+    def __str__(self):
+        return f"{self.name}"
+
 
 class SnippetPermissionSet(models.Model):
-    delete = models.BooleanField()
+    destroy = models.BooleanField()
     edit = models.BooleanField()
     edit_perms = models.BooleanField()
     fork = models.BooleanField()
     rename = models.BooleanField()
     view = models.BooleanField()
+
+    def __str__(self):
+        perms = [
+            ("delete", self.destroy),
+            ("edit", self.edit),
+            ("edit permissions", self.edit_perms),
+            ("fork", self.fork),
+            ("rename", self.rename),
+            ("view", self.view),
+        ]
+        return f"({', '.join([x[0] for x in perms if x[1]])})"
 
 
 class Snippet(models.Model):
@@ -27,8 +41,14 @@ class Snippet(models.Model):
     language_version = SemVerField(blank=True, null=True)
     code = models.TextField()
 
+    def __str__(self):
+        return f"{self.title} by {self.author.username}"
+
 
 class SnippetUserPermission(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='snippetperms')
     snippet = models.ForeignKey(Snippet, on_delete=models.CASCADE, related_name='snippetperms')
     can = SnippetPermissionSet()
+
+    def __str__(self):
+        return f"{self.user.username} can {self.can} {self.snippet.title}"
