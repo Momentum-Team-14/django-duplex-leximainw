e3a9a689d000c9ac3e044df9d94849f20cfc372d
Alexis Mainwaring <leximainw@gmail.com>
2022-08-27 13:43:35 -0400

    add snippet details page

--- core/urls.py
+++ core/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.homepage, name='Homepage'),
     path('snippets/recent/', views.snippets_recent, name='Recent Snippets'),
+    path('snippets/<int:pk>/', views.snippets_details, name='Snippet details'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from api_snippets.models import Snippet
 
 
@@ -7,5 +7,10 @@ def homepage(request):
 
 
 def snippets_recent(request):
-    snippets = Snippet.objects.order_by('created_at')[:20]
+    snippets = Snippet.objects.order_by('created_at').reverse()[:20]
     return render(request, 'core/recent_snippets_page.html', {'snippets': snippets})
+
+
+def snippets_details(request, pk=None):
+    snippet = get_object_or_404(Snippet, pk=pk)
+    return render(request, 'core/snippet_details.html', {'snippet': snippet, 'detailed': True})
