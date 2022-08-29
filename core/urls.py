a24b6b389730fc1ca7f981944905a79a0f6722c7
Alexis Mainwaring <leximainw@gmail.com>
2022-08-29 11:10:37 -0400

    add update path for editing Snippets

--- core/urls.py
+++ core/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.homepage, name='Homepage'),
     path('snippets/recent/', views.snippets_recent, name='Recent Snippets'),
-    path('snippets/new/', views.snippets_create, name='Create Snippet'),
+    path('snippets/new/', views.snippets_edit, name='Create Snippet'),
+    path('snippets/<int:pk>/edit/', views.snippets_edit, name='Edit snippet'),
     path('snippets/<int:pk>/', views.snippets_details, name='Snippet details'),
 ]
--- core/views.py
+++ core/views.py
@@ -17,24 +17,32 @@ def snippets_recent(request):
     return render(request, 'core/recent_snippets_page.html', {'snippets': recent_snippets(request.user)})
 
 
-def snippets_create(request):
+def snippets_edit(request, pk=None):
     if not request.user.is_authenticated:
         raise PermissionDenied()
     if request.method == 'POST':
-        form = SnippetForm(request.POST)
+        form = SnippetForm(request.POST, instance=get_object_or_404(Snippet, pk=pk))
         if form.is_valid():
-            snippet = form.save(commit=False)
-            snippet.author = request.user
+            if pk is None:
+                snippet = form.save(commit=False)
+                snippet.author = request.user
             return redirect('Snippet details', pk=form.save().pk)
         else:
             return redirect('/')
     else:
-        form = SnippetForm()
-        return render(request, 'core/snippet_create.html', {
+        if pk is None:
+            form = SnippetForm()
+        else:
+            form = SnippetForm(instance=get_object_or_404(Snippet, pk=pk))
+        return render(request, 'core/snippet_edit.html', {
             'form': form,
+            'button_text': 'Create' if pk is None else 'Save Changes',
         })
 
 
 def snippets_details(request, pk=None):
     snippet = get_object_or_404(Snippet, pk=pk)
-    return render(request, 'core/snippet_details.html', {'snippet': snippet, 'detailed': True})
+    return render(request, 'core/snippet_details.html', {
+        'snippet': snippet,
+        'detailed': True
+    })
