5d0073d6e3f4adc7c924298f56edf0649766cd11
Alexis Mainwaring <leximainw@gmail.com>
2022-08-31 14:49:41 -0400

    fix favorite button in profile page

--- static/js/favorites.js
+++ static/js/favorites.js
@@ -1,12 +1,15 @@
 for (let star of document.querySelectorAll('.favorite'))
 {
-    star.addEventListener('click', e => {
-        fetch(star.href)
-            .then(res => res.json())
-            .then(data => {
-                star.children[data.favorited + 0].classList.remove('hidden')
-                star.children[1 - data.favorited].classList.add('hidden')
-            })
-        e.preventDefault()
-    })
+    if(!star.is_favorite_button) {
+        star.is_favorite_button = true
+        star.addEventListener('click', e => {
+            fetch(star.href)
+                .then(res => res.json())
+                .then(data => {
+                    star.children[data.favorited + 0].classList.remove('hidden')
+                    star.children[1 - data.favorited].classList.add('hidden')
+                })
+            e.preventDefault()
+        })
+    }
 }
